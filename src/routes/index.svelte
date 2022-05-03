d68f5136eacb9a952764df95455b34fee0dfc67f
Almujtaba Abdalla <almujtaba007@gmail.com>
2022-05-03 12:00:35 +0200

    fixed formatting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,11 @@
-
-
-
 <script>
-  import Register from '../components/Register.svelte';
-  import Login from '../components/Signin.svelte';
-  let registered = false;
-  </script>
- 
+	import Register from '../components/Register.svelte';
+	import Login from '../components/Signin.svelte';
+	let registered = false;
+</script>
+
 {#if registered}
-<Login bind:registered={registered}/>
+	<Login bind:registered />
 {:else}
-<Register bind:registered={registered}/>
+	<Register bind:registered />
 {/if}
-
-
